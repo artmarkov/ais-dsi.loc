8c8ecc11995ca3cf672dd47a95e117ddf09cf5a2
artmarkov <artmarkov@mail.ru>
2021-11-22 23:51:38 +0300

    Запросы

--- common/models/education/EducationUnion.php
+++ common/models/education/EducationUnion.php
@@ -4,6 +4,7 @@ namespace common\models\education;
 
 use artsoft\behaviors\ArrayFieldBehavior;
 use common\models\studygroups\SubjectSect;
+use common\models\studyplan\Studyplan;
 use Yii;
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
@@ -92,53 +93,54 @@ class EducationUnion extends \artsoft\db\ActiveRecord
         return $this->hasMany(SubjectSect::class, ['union_id' => 'id']);
     }
 
-//    public function getSubjectById($category_id)
-//    {
-//        $data = [];
-//        if ($category_id) {
-//            $data = Subject::find()->select(['id', 'name']);
-//            foreach ($this->getSpecialityDepartments($speciality_id) as $item => $department_id) {
-//                $data->orWhere(['like', 'department_list', $department_id]);
-//
-//            }
-//            $data = $data->andFilterWhere(['like', 'category_list', $category_id]);
-//            $data = $data->andFilterWhere(['=', 'status', Subject::STATUS_ACTIVE]);
-//            $data = $data->asArray()->all();
-//        }
-//        return $data;
-//    }
-//
-//    /**
-//     * Получаем возможные дисциплины программы выбранной категории
-//
-//     */
-//    public function getSubjectByCategory($category_id)
-//    {
-//        $data = [];
-//        if ($category_id) {
-//            $data = Subject::find()->select(['name', 'id']);
-//            foreach ($this->getSpecialityDepartments($speciality_id) as $item => $department_id) {
-//                $data->orWhere(['like', 'programm_list', $department_id]);
-//
-//            }
-//            $data = $data->andFilterWhere(['like', 'programm_list', $category_id]);
-//            $data = $data->andFilterWhere(['=', 'status', Subject::STATUS_ACTIVE]);
-//            $data = $data->indexBy('id')->column();
-//        }
-//        return $data;
-//    }
-
 
     public function getSubjectByProgramList()
     {
         $data = [];
     foreach ($this->programm_list as $item => $programm_id) {
-        $data += EducationProgrammLevel::findOne(['programm_id' => $programm_id])->educationProgrammLevelSubject;
+        $models = Studyplan::findAll(['programm_id' => $programm_id]);
+                echo '<pre>' . print_r($models) . '</pre>';
+//        if($model->getStudyplanSubject()) {
+//            $studuPlan = $model->getStudyplanSubject()->asArray()->all();
+//            foreach ($studuPlan as $i => $value) {
+//                echo '<pre>' . print_r($value['subject_cat_id']) . '</pre>';
+//                echo '<pre>' . print_r($value['subject_id']) . '</pre>';
+//            }
+//        }
 //        foreach ($data as $speciality => $programm_id) {
 //            $dep = self::getSpecialityDepartments();
 //        }
     }
-        echo '<pre>' . print_r($data) . '</pre>';
+        /*SELECT distinct subject_cat_id, subject_id, guide_subject_category.name as category_name, guide_subject_category.slug as category_slug, subject.name as subject_name, subject.slug as subject_slug
+	FROM studyplan
+	inner join studyplan_subject on studyplan.id = studyplan_subject.studyplan_id
+	inner join guide_subject_category on guide_subject_category.id = studyplan_subject.subject_cat_id
+	inner join subject on subject.id = studyplan_subject.subject_id
+	where studyplan.programm_id = any (string_to_array((
+    select programm_list from education_union where id = 1000
+								   ), ',')::int[]) and subject_id is not null;
+
+    SELECT distinct subject_cat_id,  guide_subject_category.name as category_name
+	FROM studyplan
+	inner join studyplan_subject on studyplan.id = studyplan_subject.studyplan_id
+	inner join guide_subject_category on guide_subject_category.id = studyplan_subject.subject_cat_id
+	inner join subject on subject.id = studyplan_subject.subject_id
+	where studyplan.programm_id = any (string_to_array((
+    select programm_list from education_union where id = 1000
+								   ), ',')::int[])
+        and subject_id is not null;
+
+    SELECT distinct subject_id, subject.name as subject_name
+	FROM studyplan
+	inner join studyplan_subject on studyplan.id = studyplan_subject.studyplan_id
+	inner join guide_subject_category on guide_subject_category.id = studyplan_subject.subject_cat_id
+	inner join subject on subject.id = studyplan_subject.subject_id
+	where studyplan.programm_id = any (string_to_array((
+    select programm_list from education_union where id = 1000
+								   ), ',')::int[])
+        and subject_id is not null
+        and studyplan_subject.subject_cat_id = 1000;
+        */
         //sort($data);
         return $data;
     }
