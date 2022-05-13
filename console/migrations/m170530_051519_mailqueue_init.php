3ddee0fbe5084a07f557d0edb0a3bd0171985456
artmarkov <artmarkov@mail.ru>
2022-05-13 09:22:43 +0300

    Правка миграции

--- console/migrations/m170530_051519_mailqueue_init.php
+++ console/migrations/m170530_051519_mailqueue_init.php
@@ -11,6 +11,8 @@ use nterms\mailqueue\MailQueue;
  */
 class m170530_051519_mailqueue_init extends Migration
 {
+    const QUEUE_TABLE = 'mail_queue';
+
     public function up()
     {
 		$tableOptions = null;
@@ -18,24 +20,24 @@ class m170530_051519_mailqueue_init extends Migration
 			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
 		}
 		
-		$this->createTable('mail_queue', [
+		$this->createTable(self::QUEUE_TABLE, [
 			'id' => Schema::TYPE_PK,
 			'subject' => Schema::TYPE_STRING,
+			'swift_message' => Schema::TYPE_TEXT,
 			'created_at' => Schema::TYPE_DATETIME . ' NOT NULL',
+			'time_to_send' => Schema::TYPE_DATETIME . ' NOT NULL',
+			'sent_time' => Schema::TYPE_DATETIME . ' DEFAULT NULL',
 			'attempts' => Schema::TYPE_INTEGER,
 			'last_attempt_time' => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-			'sent_time' => Schema::TYPE_DATETIME . ' DEFAULT NULL',
 		], $tableOptions);
 		
-        $this->addColumn('mail_queue', 'time_to_send', $this->dateTime()->notNull());
-        $this->createIndex('IX_time_to_send', 'mail_queue', 'time_to_send');
-        $this->addColumn('mail_queue', 'swift_message', 'text');
-        $this->createIndex('IX_sent_time', 'mail_queue', 'sent_time');
-        $this->addCommentOnTable('mail_queue', 'Журнал отправленных email-сообщений');
+        $this->createIndex('IX_time_to_send', self::QUEUE_TABLE, 'time_to_send');
+        $this->createIndex('IX_sent_time', self::QUEUE_TABLE, 'sent_time');
+        $this->addCommentOnTable(self::QUEUE_TABLE, 'Журнал отправленных email-сообщений');
     }
 
     public function down()
     {
-        $this->dropTable('mail_queue');
+        $this->dropTable(self::QUEUE_TABLE);
     }
 }
