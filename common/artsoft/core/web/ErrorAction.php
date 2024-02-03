d2e0cf42d8c8016b4e05db669479bbbdcca6920e
artmarkov <artmarkov@mail.ru>
2024-02-03 11:14:33 +0300

    Переход на главную при ошибке для фронта

--- common/artsoft/core/web/ErrorAction.php
+++ common/artsoft/core/web/ErrorAction.php
@@ -2,7 +2,17 @@
 
 namespace artsoft\web;
 
+use artsoft\Art;
+use Yii;
+
 class ErrorAction extends \yii\web\ErrorAction
 {
-    
+    public function run()
+    {
+//        print_r(Yii::$app->response->getStatusCode());
+        if (Art::isFrontend()) {
+            return Yii::$app->response->redirect(Yii::$app->homeUrl);
+        }
+        return parent::run();
+    }
 }
