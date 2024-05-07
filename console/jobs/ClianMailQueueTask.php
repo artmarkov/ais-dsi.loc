3136bec05f9d9bc246f0d223313a26b0728bf8f4
artmarkov <>
2024-05-07 16:32:54 +0300

    Таск очистки очереди писем

---
+++ console/jobs/ClianMailQueueTask.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace console\jobs;
+
+use artsoft\models\UserVisitLog;
+use Yii;
+
+/**
+ * Class ClianMailQueueTask.
+ */
+class ClianMailQueueTask extends \yii\base\BaseObject implements \yii\queue\JobInterface
+{
+    public function execute($queue)
+    {
+        Yii::$app->db->createCommand('TRUNCATE TABLE mail_queue')->execute();
+    }
+
+}
--- console/migrations/m240205_116117_add_new_job.php
+++ console/migrations/m240205_116117_add_new_job.php
@@ -8,10 +8,12 @@ class m240205_116117_add_new_job extends \artsoft\db\BaseMigration
         $this->insert('{{%queue_schedule}}', ['class' => 'console\jobs\PaymentDebtorsTask', 'title' => 'Задолженность по оплате', 'content' => 'Переводит счета в статус "Задолженность по оплате" и отправляет уведомления на внутреннюю почту', 'cron_expression' => '0 2 * * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 10000, 'updated_by' => 10000]);
         $this->insert('{{%queue_schedule}}', ['class' => 'console\jobs\WorkingTimeLogTask', 'title' => 'Лог посещаемости', 'content' => 'Записывает в лог посещаемости время начала и окончания работы по расписанию и фактическое время прохода(полученя ключей) за рабочий день', 'cron_expression' => '0 23 * * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 10000, 'updated_by' => 10000]);
         $this->insert('{{%queue_schedule}}', ['class' => 'console\jobs\WorkingTimeSendTask', 'title' => 'Отправка сообщений о посещаемости', 'content' => 'Отправляет письма с информацией о посещаемости на основании информации из лога посещаемости(формируется задачей WorkingTimeLogTask) за рабочий день', 'cron_expression' => '10 23 * * 1-6', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 10000, 'updated_by' => 10000]);
+        $this->insert('{{%queue_schedule}}', ['class' => 'console\jobs\ClianMailQueueTask', 'title' => 'Очистка очереди неотправленных писем', 'content' => 'Очищает очередь неотправленных писем', 'cron_expression' => '0 3 */3 * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 10000, 'updated_by' => 10000]);
     }
 
     public function down()
     {
+        $this->delete('{{%queue_schedule}}', ['class' => 'console\jobs\ClianMailQueueTask']);
         $this->delete('{{%queue_schedule}}', ['class' => 'console\jobs\WorkingTimeSendTask']);
         $this->delete('{{%queue_schedule}}', ['class' => 'console\jobs\WorkingTimeLogTask']);
         $this->delete('{{%queue_schedule}}', ['class' => 'console\jobs\PaymentDebtorsTask']);
