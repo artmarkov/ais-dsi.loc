6f6a93ff2a07df50a8568d0e338fd4baa6c81f57
artmarkov <artmarkov@mail.ru>
2025-04-04 00:24:43 +0300

    Правки виджета

--- backend/widgets/dashboard/views/visits-student.php
+++ backend/widgets/dashboard/views/visits-student.php
@@ -4,12 +4,12 @@
 ?>
 
 <div class="panel panel-default dw-widget">
-    <div class="panel-heading">Количество посещений учеников на <?= $date ?> (согласно расписанию)</div>
+    <div class="panel-heading">Количество посещений учеников на <span style="color: #921e12"><?= $date ?></span> (согласно расписанию)</div>
     <div class="panel-body">
         <?php if ($active): ?>
             <div class="clearfix">
-                <?= '<b>Митинская 47 корп.1 (' . $active[1000]['count'] . '),</b> '?>
-                <?= '<b>Пятницкое шоссе д.40 (' . $active[1001]['count'] . ')</b> '?>
+                <?= '<b>Митинская 47 корп.1 : <span style="color: #921e12; font-size: larger">' . $active[1000]['count'] . '</span><BR/></b> '?>
+                <?= '<b>Пятницкое шоссе д.40 : <span style="color: #921e12; font-size: larger">' . $active[1001]['count'] . '</span></b> '?>
             </div>
         <?php else: ?>
             <h5><em><?= 'Посещений не планируется.' ?></em></h5>
