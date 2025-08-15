1b6fb2c86e2243f5b88c579bcf123ae7640dbfa2
artmarkov <artmarkov@mail.ru>
2025-08-15 13:19:11 +0300

    Форма 1 + столбцы

--- common/models/studyplan/StudyplanDistrib.php
+++ common/models/studyplan/StudyplanDistrib.php
@@ -31,78 +31,120 @@ class StudyplanDistrib
             ->all();
         return $models;
     }
+
     public function getData()
     {
         $data = [];
         $data['rank'] = 'doc';
         $mask = [
-            '7' => [['Фортепиано'],[]],
-            '9' => [['Баян'],[]],
-            '10' => [['Аккордеон'],[]],
-            '11' => [['Домра'],[]],
-            '12' => [['Балалайка'],[]],
-            '13' => [['Гитара'],[]],
-            '14' => [['Гусли'],[]],
-            '15' => [['Гармонь'],[]],
-            '17' => [['Флейта'],[]],
-            '18' => [['Гобой'],[]],
-            '19' => [['Кларнет'],[]],
-            '20' => [['Фагот'],[]],
-            '21' => [['Саксофон'],[]],
-            '22' => [['Труба'],[]],
-            '23' => [['Валторна'],[]],
-            '24' => [['Трамбон', 'Баритон'],[]],
-            '25' => [['Туба'],[]],
-            '26' => [['Ударные'],[]],
-            '28' => [['Скрипка'],[]],
-            '29' => [['Виолончель'],[]],
-            '30' => [['Альт'],[]],
-            '31' => [['Контрабас'],[]],
-            '32' => [['Арфа'],[]],
-            '33' => [['Ударные','Саксофон','Бас-гитара','Тромбон','Труба'],[1011,1012,1024,1025]],
-            '34' => [['Хор'],[]],
-            '35' => [['Фольклорный ансамбль'],[]],
-            '36' => [[''],[1013,1034]],
-            '37' => [[],[]],
-            '38' => [[''],[1044]],
-            '39' => [[''],[1051,1052]],
-            '40' => [[],[]],
-            '41' => [[''],[1046,1047,1048,1049]],
-            '42' => [[],[]],
-            '43' => [[''],[1033]],
-            '44' => [[],[]],
-            '46' => [[],[]],
-            '47' => [['Электрогитара'],[]],
-            '48' => [['Эстрадный вокал'],[]],
-            '49' => [['Академический вокал'],[]],
-            '50' => [[],[]],
-            '51' => [[],[]],
-            '52' => [[],[]],
+            '7' => [['Фортепиано'], []],
+            '9' => [['Баян'], []],
+            '10' => [['Аккордеон'], []],
+            '11' => [['Домра'], []],
+            '12' => [['Балалайка'], []],
+            '13' => [['Гитара'], []],
+            '14' => [['Гусли'], []],
+            '15' => [['Гармонь'], []],
+            '17' => [['Флейта'], []],
+            '18' => [['Гобой'], []],
+            '19' => [['Кларнет'], []],
+            '20' => [['Фагот'], []],
+            '21' => [['Саксофон'], []],
+            '22' => [['Труба'], []],
+            '23' => [['Валторна'], []],
+            '24' => [['Трамбон', 'Баритон'], []],
+            '25' => [['Туба'], []],
+            '26' => [['Ударные'], []],
+            '28' => [['Скрипка'], []],
+            '29' => [['Виолончель'], []],
+            '30' => [['Альт'], []],
+            '31' => [['Контрабас'], []],
+            '32' => [['Арфа'], []],
+            '33' => [['Ударные', 'Саксофон', 'Бас-гитара', 'Тромбон', 'Труба'], [1011, 1012, 1024, 1025]],
+            '34' => [['Хор'], []],
+            '35' => [['Фольклорный ансамбль'], []],
+            '36' => [[''], [1013, 1034]],
+            '37' => [[], []],
+            '38' => [[''], [1044]],
+            '39' => [[''], [1051, 1052]],
+            '40' => [[], []],
+            '41' => [[''], [1046, 1047, 1048, 1049]],
+            '42' => [[], []],
+            '43' => [[''], [1033]],
+            '44' => [[], []],
+            '46' => [[], []],
+            '47' => [['Электрогитара'], []],
+            '48' => [['Эстрадный вокал'], []],
+            '49' => [['Академический вокал'], []],
+            '50' => [[], []],
+            '51' => [[], []],
+            '52' => [[], []],
         ];
         $st = $this->getStudyplans();
-        $spec = ArrayHelper::index($st, null, ['speciality']);
+        $st_lim = array_filter(
+            $st,
+            function ($item) {
+                return array_intersect(explode(',', $item['limited_status_list']), [1000, 2000]); // С ограничениями по здоровью
+            }
+        );
+        $st_pp = array_filter(
+            $st,
+            function ($item) {
+                return $item['education_cat_programm_short_name'] == 'ПП'; // ПП Общая
+            }
+        );
+        $st_ppb = array_filter(
+            $st,
+            function ($item) {
+                return $item['education_cat_programm_short_name'] == 'ПП' && $item['subject_form_id'] == 1000; // ПП Бюджет
+            }
+        );
+        $st_op = array_filter(
+            $st,
+            function ($item) {
+                return $item['education_cat_programm_short_name'] == 'ОП'; // ОП Общая
+            }
+        );
+        $st_opb = array_filter(
+            $st,
+            function ($item) {
+                return $item['education_cat_programm_short_name'] == 'ОП' && $item['subject_form_id'] == 1000; // ОП Бюджет
+            }
+        );
+//        echo '<pre>' . print_r($st_lim, true) . '</pre>';
+//        die();
         foreach ($mask as $index => $array) {
-            $data[$index . '_all'] = 0;
+            $data[$index . '_all'] = $data[$index . '_lim'] = $data[$index . '_pp'] = 0;
+            $data[$index . '_ppb'] = $data[$index . '_op'] = $data[$index . '_opb'] = 0;
             if (!empty($array[0])) {
                 foreach ($array[0] as $value) {
-                    if (isset($spec[$value])) {
-                        if (empty($array[1])) {
-                            $data[$index . '_all'] += count($spec[$value]);
-                        } else {
-                            foreach ($spec[$value] as $val) {
-                                $data[$index . '_all'] += in_array($val['programm_id'], $array[1]) ? 1 : 0;
-                            }
-                        }
-                    }
+                    $data[$index . '_all'] = $this->setColl($data[$index . '_all'], $array, $st, $value);
+                    $data[$index . '_lim'] = $this->setColl($data[$index . '_lim'], $array, $st_lim, $value);
+                    $data[$index . '_pp'] = $this->setColl($data[$index . '_pp'], $array, $st_pp, $value);
+                    $data[$index . '_ppb'] = $this->setColl($data[$index . '_ppb'], $array, $st_ppb, $value);
+                    $data[$index . '_op'] = $this->setColl($data[$index . '_op'], $array, $st_op, $value);
+                    $data[$index . '_opb'] = $this->setColl($data[$index . '_opb'], $array, $st_opb, $value);
                 }
             }
         }
-
-       // echo '<pre>' . print_r($spec, true) . '</pre>';
-      //  echo '<pre>' . print_r($data, true) . '</pre>';
-        //echo '<pre>' . print_r($spec, true) . '</pre>';
         return $data;
     }
+
+    protected function setColl($data, $array, $st, $value)
+    {
+        $spec = ArrayHelper::index($st, null, ['speciality']);
+        if (isset($spec[$value])) {
+            if (empty($array[1])) {
+                $data += count($spec[$value]);
+            } else {
+                foreach ($spec[$value] as $val) {
+                    $data += in_array($val['programm_id'], $array[1]) ? 1 : 0;
+                }
+            }
+        }
+        return $data;
+    }
+
     /**
      * @throws \yii\base\InvalidConfigException
      */
