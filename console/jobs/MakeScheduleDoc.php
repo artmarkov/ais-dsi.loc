f4201a060576c0e3b9fd6b73aaac3b00fea25ef1
artmarkov <artmarkov@mail.ru>
2025-09-01 12:16:20 +0300

    Правки задачи выгрузки рассписаний

--- console/jobs/MakeScheduleDoc.php
+++ console/jobs/MakeScheduleDoc.php
@@ -4,8 +4,12 @@ namespace console\jobs;
 
 use artsoft\helpers\ArtHelper;
 use common\models\reports\ProgressReport;
+use common\models\teachers\TeachersConsult;
+use common\models\teachers\TeachersSchedule;
+use common\models\user\UserCommon;
 use Yii;
 use yii\base\DynamicModel;
+use yii\db\Query;
 
 /**
  * Class FlushCache.
@@ -14,13 +18,21 @@ class MakeScheduleDoc extends \yii\base\BaseObject implements \yii\queue\JobInte
 {
     public function execute($queue)
     {
-        $model_date = new DynamicModel(['plan_year', 'teachers_id', 'is_history']);
-        $model_date->addRule(['plan_year', 'teachers_id',  'is_history'], 'integer');
+        $teachersIds = (new Query())->from('teachers_view')
+            ->select('teachers_id')
+            ->where(['status' => UserCommon::STATUS_ACTIVE])
+            ->column();
+        $model_date = new DynamicModel(['plan_year', 'teachers_id']);
+        $model_date->addRule(['plan_year', 'teachers_id'], 'integer');
         $model_date->plan_year = ArtHelper::getStudyYearDefault();
-        $model_date->teachers_id = 1015;
-        $model_date->is_history = true;
-        $models = new ProgressReport($model_date);
-        $models->saveXls();
-        $models->makeDocument();
+        foreach ($teachersIds as $item => $teachers_id) {
+            $model_date->teachers_id = $teachers_id;
+            $models = new TeachersSchedule($model_date);
+            $models->saveXls();
+            $models->makeDocument();
+            $models = new TeachersConsult($model_date);
+            $models->saveXls();
+            $models->makeDocument();
+        }
     }
 }
